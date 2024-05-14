3dd0227c89c90ef33e7a5e1580b7da415cda4dcf
AbhishekS <abhi.developer28@gmail.com>
2024-05-14 19:14:52 +0530

    Added moves logic with game-board to have the buttons working.

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,6 +8,10 @@ class Game < ApplicationRecord
         self.current_symbol = [:x, :o].sample
     end
 
+    def [](row,col)
+        state[row.to_s][col.to_s]
+    end
+    
     def move!(row, col)
         # Make a mark at row/col
         state[row.to_s][col.to_s] = current_symbol.to_s
